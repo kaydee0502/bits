50d8bb6b1d0639bad1f72e45e4853b4afd82491d
Kshitij Dhama <kshitijdhama513@gmail.com>
2020-03-05 12:44:49 +0530

    WRK

---
+++ Python 3/wrkshp.py
@@ -0,0 +1,40 @@
+import random
+
+ls = []
+prime = []
+base_4 = []
+b=0
+d = 0 
+
+for _ in range(100):
+    ls.append(random.randint(2,100))
+    
+print(ls)
+    
+for i in ls:
+    a = 2
+    while a < i:
+        if i % a == 0:
+            break
+        if a == i-1:
+            prime.append(i)
+        a+=1
+    
+print(prime)
+for a in prime:
+    while a != 0:
+        b_4 = a%4
+        b = b*10 + b_4
+        a //= 4
+    while b != 0:
+        c = b%10
+        d = d*10 + c
+        b //= 10
+    b=0
+    base_4.append(d)
+    d=0
+    
+print(base_4)
+        
+
+    
